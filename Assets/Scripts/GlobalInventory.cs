b8c33e323d400d172838d2b5b1d11874798d9000
Tân Phạm <tanpham2421@gmail.com>
2025-11-14 16:12:01 +0700

    add FlashLight item

--- Assets/Scripts/GlobalInventory.cs
+++ Assets/Scripts/GlobalInventory.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 public class GlobalInventory : MonoBehaviour
 {
     public static bool hasKey = false;
+    public static bool hasGuardKey = false;
     public static bool hasVHSTape = false;
+    public static bool hasFlashlight = false;
+
+    // Chỉ cho nhặt flashlight sau khi xem xong băng cassette
+    public static bool canPickupFlashlight = false;
 }
